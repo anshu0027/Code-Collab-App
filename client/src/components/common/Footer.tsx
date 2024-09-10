f81d54da86d6b1bfbce0c991cdf67eee90facd30
anshu0027 <patelanshu0027@gmail.com>
2024-09-10 18:12:25 +0530

    first commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by Anshu Patel, Harshil Patel, Vedant Patel
+                <a
+                    href="#"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Group Project   
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
